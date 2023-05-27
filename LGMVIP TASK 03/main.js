6479d3e5c27fc3ba5f0a0b7bdbd6ccf965e840f7
Pavan badgujar <113707864+PavanBadgujar1137@users.noreply.github.com>
2023-05-27 00:46:09 -0700

    first changes

---
+++ LGMVIP TASK 03/main.js
@@ -0,0 +1,19 @@
+const buttons = document.querySelectorAll(".grid button");
+const display = document.querySelector(".display");
+buttons.forEach(function (button) {
+  button.addEventListener("click", function (e) {
+    if (e.target.innerText == "=") {
+      try {
+        display.innerText = eval(display.innerText);
+      } catch {
+        display.innerText = "Invalid";
+      }
+    } else if (e.target.innerText == "C") {
+      display.innerText = "";
+    } else if (e.target.innerText == "←") {
+      display.innerText = display.innerText.slice(0, -1);
+    } else {
+      display.innerText += e.target.innerText;
+    }
+  });
+});
